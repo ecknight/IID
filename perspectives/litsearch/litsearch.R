14546b492aa60c3c39bc1795c0fcba7b0e35b6d3
ecknight <ellyknight@gmail.com>
2023-04-06 09:33:06 -0600

    Fix category classification

--- perspectives/litsearch/litsearch.R
+++ perspectives/litsearch/litsearch.R
@@ -10,7 +10,7 @@ root <- "G:/.shortcut-targets-by-id/14H5BXdBP8k2jv4jgUjO5QTqHvOmxMSBn/Individual
 #acoustic AND "individual identification"  (Topic) AND wildlife OR bird OR mammal OR amphibian OR insect OR fish  (Topic) Timespan:1864-2023. 
 
 #2. Import results----
-naiveimport <- litsearchr::import_results(file=file.path(root, "savedrecs.txt"))
+naiveimport <- litsearchr::import_results(file=file.path(root, "iid_naivesearch.txt"))
 
 #3. Remove duplicates----
 #Shouldn't be any because I only used one search engine...
@@ -60,9 +60,63 @@ searchterms <- litsearchr::get_keywords(reducedgraph)
 
 #7. Manual review----
 searchterms
-use <- c(0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0)
+group <- c(0,0,1,1,0,0,1,NA,0,0,0,0,0,1,1,1,1,1,0,0,NA,NA,0)
 terms <- data.frame(terms = searchterms,
-                    use = use)
-View(terms)
+                    group = group)
+terms %>% 
+       dplyr::filter(!is.na(group))
+
+mysearchterms <- list(dplyr::filter(terms, group==1)$terms,
+                      dplyr::filter(terms, group==0)$terms)
 
 #8. Write out the ones to use----
+my_search <-
+  litsearchr::write_search(
+    groupdata = mysearchterms,
+    languages = "English",
+    stemming = TRUE,
+    closure = "none",
+    exactphrase = TRUE,
+    writesearch = FALSE,
+    verbose = TRUE
+  )
+
+#9. Choose some gold standard references----
+gold_standard <- read.csv(file.path(root, "goldstandard.csv"))$Title
+
+#10. Go do the search----
+#https://www-webofscience-com.login.ezproxy.library.ualberta.ca/wos/alldb/summary/3037af31-ebc2-472f-a182-76600350a9bd-7f323585/relevance/1
+
+#11. Read in the results----
+retrieved_articles1 <- litsearchr::import_results(file=file.path(root, "iid_fullsearch", "iid_fullsearch1.txt"))
+retrieved_articles2 <- litsearchr::import_results(file=file.path(root, "iid_fullsearch", "iid_fullsearch2.txt"))
+retrieved_articles <- data.table::rbindlist(list(retrieved_articles1, retrieved_articles2), fill=TRUE)
+retrieved_articles <- remove_duplicates(retrieved_articles, field="title", method="string_osa")
+
+#12. Check recall----
+articles_found <- check_recall(true_hits=gold_standard,
+                               retrieved = retrieved_articles$title) %>% 
+  data.frame() %>% 
+  arrange(desc(Similarity))
+
+write.csv(articles_found, file.path(root, "goldstandard_recall.csv"), row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
