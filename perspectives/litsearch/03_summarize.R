d51d2db50982d1da9662d6358e975eb2ee21c083
ecknight <ellyknight@gmail.com>
2023-09-25 09:52:30 -0600

    Initial commit

---
+++ perspectives/litsearch/03_summarize.R
@@ -0,0 +1,8 @@
+#1. Packages----
+
+library(tidyverse)
+library(googlesheets4)
+library(googledrive)
+
+#2. Files location----
+root <- "G:/.shortcut-targets-by-id/14H5BXdBP8k2jv4jgUjO5QTqHvOmxMSBn/IndividualID/PerspectivesPaper/"
